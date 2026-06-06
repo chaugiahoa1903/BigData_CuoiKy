02a06092a26ae4c6b4a1f65951640a4583ab1c4a
QnX-Bits <tuananhh231@gmail.com>
2026-06-06 18:35:10 +0700

    clean data and merge dataframe

--- src/01_data_preparation_and_eda.py
+++ src/01_data_preparation_and_eda.py
@@ -22,3 +22,33 @@ def load_data(spark):
     df_sales  = df_sales_spark.toPandas()
     df_stores = df_stores_spark.toPandas()
     return df_sales, df_stores
+# Làm sạch dữ liệu và join stores vào sales
+#Ở CompetitionDistance thay thế giá trị thiếu bằng giá trị trung bình.
+#Ở Promo2 thay thế giá trị thiếu bằng 0.
+#chuyển cột Date thành định dạng datetime và join DataFrame dựa trên cột Store.
+def clean_and_join(df_sales: pd.DataFrame,
+                   df_stores: pd.DataFrame) -> pd.DataFrame:
+    
+    print("\n── Bộ dữ liệu Sales ──")
+    print(df_sales.shape)
+    print(df_sales.isnull().sum())
+
+    print("\n── Bộ dữ liệu Stores ──")
+    print(df_stores.shape)
+    print(df_stores.isnull().sum())
+
+    df_stores["CompetitionDistance"] = df_stores[
+        "CompetitionDistance"
+    ].fillna(df_stores["CompetitionDistance"].mean())
+
+
+    df_stores["Promo2"] = df_stores["Promo2"].fillna(0)
+
+   
+    df_sales["Date"] = pd.to_datetime(df_sales["Date"])
+    df = df_sales.merge(right=df_stores, on="Store", how="left")
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    print(f"\n Joined shape: {df.shape}")
+    print(df.head())
+    return df
