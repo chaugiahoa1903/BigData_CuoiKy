904e6cb2c1333f31a9202f49e5a06b999cf746e4
khangle-16 <khangle16052005@gmail.com>
2026-06-08 13:34:49 +0700

    FE: Xử lý các biến datetime, thời gian

--- src/02_feature_engineering.py
+++ src/02_feature_engineering.py
@@ -4,3 +4,13 @@ import pandas as pd
 df = pd.read_csv("../data/df_clean.csv")
 df["Date"] = pd.to_datetime(df["Date"])
 print("df_clean:", df.shape)
+
+df ["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values(by=["Store","Date"]).reset_index(drop=True)
+
+df["Year"]      = df["Date"].dt.year
+df["Month"]     = df["Date"].dt.month
+df["WeekOfYear"]= df["Date"].dt.isocalendar().week.astype(int)
+df["DayOfWeek"] = df["Date"].dt.dayofweek + 1
+df["IsWeekend"] = df["DayOfWeek"].isin([6, 7]).astype(int)
+
